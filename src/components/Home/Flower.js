c75b089f73def88d1e42860afd097e0d5caeb209
Boris Civcic <civcicboris93@gmail.com>
2019-07-20 16:27:11 +0200

    add: Flowers grid

--- src/components/Home/Flower.js
+++ src/components/Home/Flower.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
-
+import {
+  Col, Row
+} from 'reactstrap'
+import Container from '../../common/container/Container'
 class Flower extends Component {
   render() {
     console.log('items', this.props.flowers.result)
     return (
-      <div>
-        <ul>
+      <Container>
+        <Row>
           {this.props.flowers && this.props.flowers.result && this.props.flowers.result.flowers ? this.props.flowers.result.flowers.map(item => {
             return (
-              <li key={item.id}>{item.name}</li>
+              <Col lg={3} md={4} sm={6} key={item.id} className="flower-item">
+                <div className="flower-item-info">
+                  <img src={item.profile_picture} alt={item.latin_name} />
+                  <div className="flower-info">
+                    <h2>{item.name}</h2>
+                    <h3>{item.latin_name}</h3>
+                    <span>{item.sightings} sightings</span>
+                  </div>
+                </div>
+              </Col>
             )
           }) : null}
-        </ul>
-      </div>
+        </Row>
+      </Container>
     )
   }
 }
